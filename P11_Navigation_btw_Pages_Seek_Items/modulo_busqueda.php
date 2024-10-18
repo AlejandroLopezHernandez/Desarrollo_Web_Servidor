17fc97a373987832abe7c56e8aa710bf2cb56cb1
Alejandro Lopez <alejal07@ucm.es>
2024-10-18 13:37:49 +0200

    subiendo cambios de la práctica 11

--- P11_Navigation_btw_Pages_Seek_Items/modulo_busqueda.php
+++ P11_Navigation_btw_Pages_Seek_Items/modulo_busqueda.php
@@ -8,8 +8,12 @@ include 'modulo_datos.php';
         /*La función strpos devuelve la posición de la primera aparición de una
         subcadena, empezando desde 0, si no la encuentra, da false. Nota: es case sensitive*/
         foreach ($productos as $id => $producto) { 
-            if (strpos(strtolower($_GET['busqueda']), $busqueda) !== 
-false || strpos(strtolower($_GET['busqueda']), $busqueda) !== false) { 
+        /*Hacemos un bucle for each para recorrer los productos que tenemos en nuestra BBDD, y con
+        la variable $producto accedemos a los detalles de ese producto. $productos es el nombre de nuestra BBDD.
+        Para meternos dentro del array de productos, hay que usar $id para ir por cada
+        producto y luego usar $producto para meternos dentro del contenido de cada producto.*/
+            if (strpos(strtolower($producto['nombre']), $busqueda) !== false 
+            || strpos(strtolower($producto['categoria']), $busqueda) !== false) { 
         echo "<li><a href='detalles.php?producto=$id'> 
         Ver detalles {$producto['nombre']} - {$producto['categoria']} 
         </a></li>"; 
