d55c81fef75e2cfb0956e9c5b5566f7ad3345250
Alejandro Lopez <alejal07@ucm.es>
2024-10-22 11:10:14 +0200

    comentando el código

--- P12_Role_Based_Access_Control_Redirection/login.php
+++ P12_Role_Based_Access_Control_Redirection/login.php
@@ -2,17 +2,21 @@
 session_start();
 include 'modulo_roles.php';
 
-//1.- Obtener credenciales 
+//1.- Obtener credenciales con $_POST
 $user = $_POST['username'];
 $password = $_POST['password'];
 $credenciales_correctas = false;
-//Para meterse dentro de un array, creamos una variable y la igualamos a un array
-//$array1 = $usuarios;
-//Después nos metemos otra vez dentro del array con el valor entre corchetes
-//$array2 = $array1["admin"];
+/*Para meterse dentro de un array, creamos una variable y la igualamos a un array, 
+con esto nos metemos dentro de la base de datos
+$array1 = $usuarios;
+Después nos metemos otra vez dentro del array con el valor entre corchetes
+$array2 = $array1["admin"];
 
+//Con este bucle foreach, declaramos $usuarios, que es nuestra base de datos, con $usuario ya accedemos a un objeto
+de dicha base de datos, y con la $clave['elemento'] accedemos a los elementos de esta base de datos 
+*/
 foreach ($usuarios as $usuario => $clave) {
-    //verificación de credenciales
+    //2.-Verificación de credenciales
     if ($usuario === $user && $password === $clave['password']) {
         $credenciales_correctas = true;
         //3.- Redirigir según el rol 
